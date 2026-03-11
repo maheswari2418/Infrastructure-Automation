1beab6e2b0f9e88ed72ea22db50d42560bf25ad4
Maheswari Kosaraju <151656125+maheswari2418@users.noreply.github.com>
2026-03-11 20:48:41 +0530

    Change web servers

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_subnet" "web_2" {
 
   tags = {
     Name = "${var.environment}-web-subnet-2"
-    Tier = "Web"
+    Tier = "Web1"
   }
 }
 
@@ -511,4 +511,5 @@ resource "aws_s3_bucket_public_access_block" "main" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
 
